adc9936468e0a271776cc82e5ffe903b4fe448d6
notsure55 <blackender5123@mailfence.com>
2025-05-09 13:42:01 -0700

    Update TrampolineHook.c

--- Src/Hook/TrampolineHook.c
+++ Src/Hook/TrampolineHook.c
@@ -5,7 +5,6 @@
 
 #include <windows.h>
 
-#define HOOK_SIZE 15
 #define JMP_SIZE 12
 
 bool resolve_func_address(void** address)
@@ -29,11 +28,66 @@ bool resolve_func_address(void** address)
 
         *address = (void*) ((size_t)(*address) + relative_offset);
 
-        printf("Absolute address of function prologue: 0x%08x \n", (size_t)(*address));
+        printf("Absolute address of function prologue: 0x%zx \n", (size_t)(*address));
 
         return true;
     }
     return false;
+}
+
+const unsigned char rip_relative_opcodes[3] = { 0xE9, 0xE8, 0x0D };
+const unsigned char opcodes[2] = { 0x48, 0x8D };
+
+// going to be working on this to handle any situation, though will probaly take a long time.
+size_t calculate_prologue_size(void** address)
+{
+    unsigned char bytes[30];
+    DWORD old_protect = change_protections(*address, sizeof(bytes), PAGE_EXECUTE_READ);
+
+    // copy bytes from function prologue
+    memcpy(bytes, *address, sizeof(bytes));
+
+    size_t current_size = 0;
+
+    for (size_t i = 0; i < sizeof(bytes); ++i)
+    {
+        for (size_t j = 0; j < sizeof(rip_relative_opcodes); ++j)
+        {
+            if (bytes[i] == rip_relative_opcodes[j])
+            {
+                printf("Found a rip relative instruction at 0x%zx\n", ((size_t)(*address)) + i);
+            }
+        }
+
+        for (size_t j = 0; j < sizeof(opcodes); ++j)
+        {
+            if (bytes[i] == opcodes[j])
+            {
+                switch (bytes[i]) {
+                case 0x48:
+                    current_size += i;
+                    if (current_size >= JMP_SIZE)
+                    {
+                        change_protections(*address, sizeof(bytes), old_protect);
+                        return (current_size - 1);
+                    }
+                    break;
+
+                case 0xD0:
+                    printf("Found a LEA rip relative instruction at 0x%zx\n", ((size_t)(*address)) + i);
+                    break;
+
+                default:
+                    break;
+
+                }
+            }
+        }
+    }
+
+    // this is impossible
+    printf("30 bytes was not enough!\n");
+    return 0;
 }
 
 void trampoline_hook(void* src, void* dst, void** trampoline_function)
@@ -42,19 +96,24 @@ void trampoline_hook(void* src, void* dst, void** trampoline_function)
     unsigned char* jmp_bytes = NULL;
     create_jump_bytes(&jmp_bytes, dst);
 
-    // for storing overwritten bytes
-    unsigned char saved_bytes[HOOK_SIZE];
-
     // RESOLVE_FUNC
     if (!resolve_func_address(&src))
     {
         printf("Address is not apart of the iat!\n");
     }
 
-    DWORD old_protect = change_protections(src, HOOK_SIZE, PAGE_EXECUTE_READWRITE);
+    // calculate size of bytes we wont to overwrite
+    const size_t hook_size = calculate_prologue_size(&src);
+
+    printf("Calculated hook_size: %zu\n", hook_size);
+
+    // for storing overwritten bytes
+    unsigned char *saved_bytes = (unsigned char*)malloc(hook_size);
+
+    DWORD old_protect = change_protections(src, hook_size, PAGE_EXECUTE_READWRITE);
 
     // save bytes
-    memcpy(saved_bytes, src, HOOK_SIZE);
+    memcpy(saved_bytes, src, hook_size);
     // overwrite bytes with jmp
     memcpy(src, jmp_bytes, JMP_SIZE);
 
@@ -63,24 +122,24 @@ void trampoline_hook(void* src, void* dst, void** trampoline_function)
 
     printf("Hook installed at: %zx\n", (size_t)src);
 
-    change_protections(src, HOOK_SIZE, old_protect);
+    change_protections(src, hook_size, old_protect);
 
-    *trampoline_function = VirtualAlloc(NULL, HOOK_SIZE + JMP_SIZE, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    *trampoline_function = VirtualAlloc(NULL, hook_size + JMP_SIZE, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
 
     // write saved bytes to new alloced memory for trampoline_func
-    memcpy(*trampoline_function, saved_bytes, HOOK_SIZE);
-    FlushInstructionCache(GetCurrentProcess(), *trampoline_function, HOOK_SIZE);
+    memcpy(*trampoline_function, saved_bytes, hook_size);
+    FlushInstructionCache(GetCurrentProcess(), *trampoline_function, hook_size);
 
     unsigned char* jmp_to_src_bytes = NULL;
-    create_jump_bytes(&jmp_to_src_bytes, (void*)(((size_t)src) + HOOK_SIZE));
+    create_jump_bytes(&jmp_to_src_bytes, (void*)(((size_t)src) + hook_size));
 
-    size_t trampoline_address = ((size_t)(*trampoline_function)) + HOOK_SIZE;
+    size_t trampoline_address = ((size_t)(*trampoline_function)) + hook_size;
 
     // write jmp back to src function, to resume execution
     memcpy((void*)trampoline_address, jmp_to_src_bytes, JMP_SIZE);
     FlushInstructionCache(GetCurrentProcess(), (void*)trampoline_address, JMP_SIZE);
 
-    printf("Trampoline Setup at: %zx\n", trampoline_address - HOOK_SIZE);
+    printf("Trampoline Setup at: %zx\n", trampoline_address - hook_size);
 
     // cleanup
     free(jmp_bytes);
